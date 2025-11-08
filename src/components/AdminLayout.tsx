c8b5a7edcb5c24d9299a60a69e96e9ccb9cce308
[dyad] <inovaceg@gmail.com>
2025-11-08 20:22:00 -0300

    [dyad] wrote 1 file(s)

--- src/components/AdminLayout.tsx
+++ src/components/AdminLayout.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link, Outlet, useNavigate } from 'react-router-dom';
 import { Button } from '@/components/ui/button';
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
