ac8019cac0f760448fcd8f73380ccf7c8d9f89f5
[dyad] <inovaceg@gmail.com>
2025-11-09 02:01:14 -0300

    [dyad] Corrigi o erro "React.Children.only" em App.tsx, envolvendo todos os filhos do QueryClientProvider em um React.Fragment. - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -21,34 +21,36 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <Toaster /> {/* Movido para fora do TooltipProvider */}
-    <Sonner /> {/* Movido para fora do TooltipProvider */}
-    <TooltipProvider>
-      <BrowserRouter> {/* Agora BrowserRouter é o único filho direto do TooltipProvider */}
-        <SessionContextProvider>
-          <Routes>
-            {/* Rotas Públicas */}
-            <Route path="/" element={<Index />} />
-            <Route path="/historia" element={<Historia />} />
-            <Route path="/produtos" element={<Produtos />} />
-            <Route path="/qualidade" element={<Qualidade />} />
-            <Route path="/contato" element={<Contato />} />
-            <Route path="/orcamento" element={<Orcamento />} />
-            <Route path="/login" element={<Login />} />
+    <React.Fragment> {/* Adicionado React.Fragment para envolver todos os filhos do QueryClientProvider */}
+      <Toaster />
+      <Sonner />
+      <TooltipProvider>
+        <BrowserRouter>
+          <SessionContextProvider>
+            <Routes>
+              {/* Rotas Públicas */}
+              <Route path="/" element={<Index />} />
+              <Route path="/historia" element={<Historia />} />
+              <Route path="/produtos" element={<Produtos />} />
+              <Route path="/qualidade" element={<Qualidade />} />
+              <Route path="/contato" element={<Contato />} />
+              <Route path="/orcamento" element={<Orcamento />} />
+              <Route path="/login" element={<Login />} />
 
-            {/* Rotas Administrativas */}
-            <Route path="/admin" element={<AdminLayout />}>
-              <Route index element={<AdminDashboard />} />
-              <Route path="products" element={<AdminProducts />} />
-              {/* Futuras rotas de admin serão adicionadas aqui */}
-            </Route>
+              {/* Rotas Administrativas */}
+              <Route path="/admin" element={<AdminLayout />}>
+                <Route index element={<AdminDashboard />} />
+                <Route path="products" element={<AdminProducts />} />
+                {/* Futuras rotas de admin serão adicionadas aqui */}
+              </Route>
 
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </SessionContextProvider>
-      </BrowserRouter>
-    </TooltipProvider>
+              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </SessionContextProvider>
+        </BrowserRouter>
+      </TooltipProvider>
+    </React.Fragment> {/* Fechamento do React.Fragment */}
   </QueryClientProvider>
 );
 
